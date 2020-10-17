2758dc5124fe844853c5f39958cd5f2976f1e725
HereticHorse <leena.karjalainen92@gmail.com>
2020-10-17 14:54:10 +0300

    enemies and background
    creating repeating backgroung also graphincs are done by myself!
    next stop updating game graphics

--- endlessRunner/EndlessRunner/Assets/player.cs
+++ endlessRunner/EndlessRunner/Assets/player.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class player : MonoBehaviour
 {
   
@@ -13,21 +13,31 @@ public class player : MonoBehaviour
     public float maxHeight;
     public float minHeight;
 
+ 
     //health
 
     public int health = 3;
+    public GameObject effect;
 
 
     // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+            
+            Instantiate(effect, transform.position, Quaternion.identity);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);   
         if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2( transform.position.x , transform.position.y +  Yincrement); 
+            
                  
         }    else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
         {
+            
             targetPos = new Vector2( transform.position.x , transform.position.y - Yincrement);
                  
         }
